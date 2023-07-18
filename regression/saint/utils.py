ee76b2a9cb41e8b9a31aa3fe9b31d6844d70f7f2
Jungmin-YUN-0 <76892989+Jungmin-YUN-0@users.noreply.github.com>
2023-07-18 18:01:50 +0900

    Update utils.py

--- regression/saint/utils.py
+++ regression/saint/utils.py
@@ -1,7 +1,6 @@
 import torch
 from sklearn.metrics import roc_auc_score, mean_squared_error, r2_score, accuracy_score, mean_absolute_error, mean_squared_log_error
 import numpy as np
-from augmentations import embed_data_mask
 import torch.nn as nn
 import pandas as pd
 
@@ -13,7 +12,6 @@ def make_default_mask(x):
 def tag_gen(tag,y):
     return np.repeat(tag,len(y['data']))
 
-
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)  
 
@@ -24,6 +22,38 @@ def get_scheduler(args, optimizer):
         scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
                                       milestones=[args.epochs // 2.667, args.epochs // 1.6, args.epochs // 1.142], gamma=0.1)
     return scheduler
+
+def embed_data_mask(x_categ, x_cont, cat_mask, con_mask,model, vision_dset=False):
+    device = x_cont.device
+    x_categ = x_categ + model.categories_offset.type_as(x_categ)
+    x_categ_enc = model.embeds(x_categ)
+    n1,n2 = x_cont.shape
+    _, n3 = x_categ.shape
+    if model.cont_embeddings == 'MLP':
+        x_cont_enc = torch.empty(n1,n2, model.dim)
+        for i in range(model.num_continuous):
+            x_cont_enc[:,i,:] = model.simple_MLP[i](x_cont[:,i])
+    else:
+        raise Exception('This case should not work!')    
+    #image_feature_enc = model.embeds(image_feature)
+
+    x_cont_enc = x_cont_enc.to(device)
+    cat_mask_temp = cat_mask + model.cat_mask_offset.type_as(cat_mask)
+    con_mask_temp = con_mask + model.con_mask_offset.type_as(con_mask)
+
+    cat_mask_temp = model.mask_embeds_cat(cat_mask_temp)
+    con_mask_temp = model.mask_embeds_cont(con_mask_temp)
+    x_categ_enc[cat_mask == 0] = cat_mask_temp[cat_mask == 0]
+    x_cont_enc[con_mask == 0] = con_mask_temp[con_mask == 0]
+
+    if vision_dset:
+        
+        pos = np.tile(np.arange(x_categ.shape[-1]),(x_categ.shape[0],1))
+        pos =  torch.from_numpy(pos).to(device)
+        pos_enc =model.pos_encodings(pos)
+        x_categ_enc+=pos_enc
+
+    return x_categ, x_categ_enc, x_cont_enc#, image_feature_enc
 
 def imputations_acc_justy(model,dloader,device):
     model.eval()
