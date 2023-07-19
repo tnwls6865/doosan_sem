e2bc1e70feb784b3286774111f96d894b650fa40
Jungmin-YUN-0 <76892989+Jungmin-YUN-0@users.noreply.github.com>
2023-07-19 11:27:05 +0900

    Update train.py

--- regression/saint/train.py
+++ regression/saint/train.py
@@ -13,7 +13,6 @@ from utils import count_parameters, mean_sq_error, embed_data
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--vision_dset', default="False")
 parser.add_argument('--feature_num', default= "1", choices=["1","2","3","4","5","6"])
 
 parser.add_argument('--cont_embeddings', default='MLP', type=str,choices = ['MLP','Noemb'])
@@ -27,7 +26,7 @@ parser.add_argument('--attentiontype', default='colrow', type=str,choices = ['co
 parser.add_argument('--optimizer', default='AdamW', type=str,choices = ['AdamW','Adam','SGD'])
 parser.add_argument('--scheduler', default='cosine', type=str,choices = ['cosine','linear'])
 parser.add_argument('--lr', default=0.0001, type=float)
-parser.add_argument('--epochs', default=200, type=int)
+parser.add_argument('--epochs', default=100, type=int)
 parser.add_argument('--batchsize', default=216, type=int)
 parser.add_argument('--savemodelroot', default='./bestmodels', type=str)
 parser.add_argument('--run_name', default='testrun', type=str)
@@ -38,8 +37,9 @@ opt = parser.parse_args()
 
 modelsave_path = os.path.join(os.getcwd(),opt.savemodelroot,opt.run_name)
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-print(f"Device is {device}.")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+#print(f"Device is {device}.")
 
 torch.manual_seed(opt.set_seed)
 os.makedirs(modelsave_path, exist_ok=True)
@@ -94,9 +94,8 @@ ff_dropout = opt.ff_dropout,
 mlp_hidden_mults = (4, 2),       
 cont_embeddings = opt.cont_embeddings,
 attentiontype = opt.attentiontype,
-y_dim = y_dim
+y_dim=y_dim,
 )
-vision_dset = opt.vision_dset
 
 criterion = nn.MSELoss().to(device)
 
@@ -128,63 +127,64 @@ ACC_best_test_r2 = -100000
 RMSLE_best_test_ratio = 0
 ACC_best_test_ratio = 0
 
-print('Training begins now.')
-for epoch in range(opt.epochs):
-    model.train()
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        optimizer.zero_grad()
-        x_categ, x_cont, y_gts, image_feature = data[0].to(device), data[1].to(device),data[2].to(device), data[5].to(device)
+if __name__ == '__main__':
+    print('Training begins now.')
+    for epoch in range(opt.epochs):
+        model.train()
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            optimizer.zero_grad()
+            x_categ, x_cont, y_gts, image_feature = data[0].to(device), data[1].to(device),data[2].to(device), data[3].to(device)
 
-        # convert the data to embeddings in the next step
-        _ , x_categ_enc, x_cont_enc = embed_data(x_categ, x_cont, model,vision_dset) 
-                  
-        reps = model.transformer(x_categ_enc, x_cont_enc, image_feature)
-        # select only the representations corresponding to CLS token and apply mlp on it in the next step to get the predictions.
-        y_reps = reps[:,0,:]
-        y_outs = model.mlpfory(y_reps)
+            # convert the data to embeddings in the next step
+            _ , x_categ_enc, x_cont_enc = embed_data(x_categ, x_cont, model) 
+                    
+            reps = model.transformer(x_categ_enc, x_cont_enc, image_feature)
+            # select only the representations corresponding to CLS token and apply mlp on it in the next step to get the predictions.
+            y_reps = reps[:,0,:]
+            y_outs = model.mlpfory(y_reps)
 
-        loss = criterion(y_outs,y_gts) 
-        loss.backward()
-        optimizer.step()
-        if opt.optimizer == 'SGD':
-            scheduler.step()
-        running_loss += loss.item()
+            loss = criterion(y_outs,y_gts) 
+            loss.backward()
+            optimizer.step()
+            if opt.optimizer == 'SGD':
+                scheduler.step()
+            running_loss += loss.item()
 
-    if opt.active_log=='True':
-        wandb.log({'epoch': epoch ,'train_epoch_loss': running_loss, 
-        'loss': loss.item()
-        })
+        if opt.active_log=='True':
+            wandb.log({'epoch': epoch ,'train_epoch_loss': running_loss, 
+            'loss': loss.item()
+            })
 
-    if epoch%1==0:
-            model.eval()
-            with torch.no_grad():
+        if epoch%1==0:
+                model.eval()
+                with torch.no_grad():
 
-                valid_mae,valid_mse,valid_rmsle,valid_r2, valid_y_test,valid_y_pred, valid_ratio = mean_sq_error(model, validloader, device,vision_dset)    
-                test_mae,test_mse,test_rmsle,test_r2, test_y_test,test_y_pred, test_ratio = mean_sq_error(model, testloader, device,vision_dset)  
+                    valid_mae,valid_mse,valid_rmsle,valid_r2, valid_y_test,valid_y_pred, valid_ratio = mean_sq_error(model, validloader, device)    
+                    test_mae,test_mse,test_rmsle,test_r2, test_y_test,test_y_pred, test_ratio = mean_sq_error(model, testloader, device)  
 
-                print('[EPOCH %d] VALID | %.3f | RMSE: %.3f / MAE: %.3f / MSE: %.3f / R2: %.3f / ACC: %.3f' %(epoch + 1, valid_ratio, valid_rmsle,valid_mae,valid_mse,valid_r2,valid_ratio ))
-                print('[EPOCH %d]  TEST | %.3f | RMSE: %.3f / MAE: %.3f / MSE: %.3f / R2: %.3f / ACC: %.3f' %(epoch + 1, test_ratio, test_rmsle,test_mae,test_mse,test_r2,test_ratio ))
-                
-                if opt.active_log=='True':
-                    wandb.log({'valid_rmsle': valid_rmsle ,'test_rmsle': test_rmsle })     
-                if valid_rmsle < best_valid_rmsle:
-                    best_valid_rmsle = valid_rmsle
-                    RMSLE_best_test_rmsle = test_rmsle
-                    RMSLE_best_test_mae = test_mae
-                    RMSLE_best_test_r2 = test_r2
-                    RMSLE_best_test_ratio = test_ratio
-                    torch.save(model.state_dict(),'%s/bestmodel.pth' % (modelsave_path))
-                if valid_ratio > best_valid_ratio:
-                    best_valid_ratio = valid_ratio
-                    ACC_best_test_rmsle = test_rmsle
-                    ACC_best_test_mae = test_mae
-                    ACC_best_test_r2 = test_r2
-                    ACC_best_test_ratio = test_ratio
-            model.train()
+                    print('[EPOCH %d] VALID | %.3f | RMSE: %.3f / MAE: %.3f / MSE: %.3f / R2: %.3f / ACC: %.3f' %(epoch + 1, valid_ratio, valid_rmsle,valid_mae,valid_mse,valid_r2,valid_ratio ))
+                    print('[EPOCH %d]  TEST | %.3f | RMSE: %.3f / MAE: %.3f / MSE: %.3f / R2: %.3f / ACC: %.3f' %(epoch + 1, test_ratio, test_rmsle,test_mae,test_mse,test_r2,test_ratio ))
+                    
+                    if opt.active_log=='True':
+                        wandb.log({'valid_rmsle': valid_rmsle ,'test_rmsle': test_rmsle })     
+                    if valid_rmsle < best_valid_rmsle:
+                        best_valid_rmsle = valid_rmsle
+                        RMSLE_best_test_rmsle = test_rmsle
+                        RMSLE_best_test_mae = test_mae
+                        RMSLE_best_test_r2 = test_r2
+                        RMSLE_best_test_ratio = test_ratio
+                        torch.save(model.state_dict(),'%s/bestmodel.pth' % (modelsave_path))
+                    if valid_ratio > best_valid_ratio:
+                        best_valid_ratio = valid_ratio
+                        ACC_best_test_rmsle = test_rmsle
+                        ACC_best_test_mae = test_mae
+                        ACC_best_test_r2 = test_r2
+                        ACC_best_test_ratio = test_ratio
+                model.train()
 
 
-total_parameters = count_parameters(model)
-print('TOTAL NUMBER OF PARAMS: %d' %(total_parameters))
-print(f"RMSLE-based || MAE:{RMSLE_best_test_mae} | RMSLE:{RMSLE_best_test_rmsle} | R2:{RMSLE_best_test_r2} | ACC:{RMSLE_best_test_ratio}")
-print(f"ACC-based || MAE:{ACC_best_test_mae} | RMSLE:{ACC_best_test_rmsle} | R2:{ACC_best_test_r2} | ACC:{ACC_best_test_ratio}")
+    total_parameters = count_parameters(model)
+    print('TOTAL NUMBER OF PARAMS: %d' %(total_parameters))
+    print(f"RMSLE-based || MAE:{RMSLE_best_test_mae} | RMSLE:{RMSLE_best_test_rmsle} | R2:{RMSLE_best_test_r2} | ACC:{RMSLE_best_test_ratio}")
+    print(f"ACC-based || MAE:{ACC_best_test_mae} | RMSLE:{ACC_best_test_rmsle} | R2:{ACC_best_test_r2} | ACC:{ACC_best_test_ratio}")
